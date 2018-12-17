83262a3b5e6c7e5d1d0dc86584aece00c823ed10
杨淮生 <codefan@centit.com>
2018-12-17 10:46:54 +0800

    add ch07 convnet neural network

--- deepLearning/common/layers.py
+++ deepLearning/common/layers.py
@@ -110,14 +110,14 @@ class Dropout:
     def __init__(self, dropout_ratio=0.5):
         self.dropout_ratio = dropout_ratio
         self.mask = None
-
+    # 按照比例随机删除部分 神经元
     def forward(self, x, train_flg=True):
         if train_flg:
             self.mask = np.random.rand(*x.shape) > self.dropout_ratio
             return x * self.mask
         else:
             return x * (1.0 - self.dropout_ratio)
-
+    # 删除的神经元 不传递 偏导数（导数设置为 0 ，这样对应的dw db就不会修改）
     def backward(self, dout):
         return dout * self.mask
 
