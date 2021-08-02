f4bb424acef0584670a68c6b6bdca8cd9ed7c243
codefan <codefan@sina.com>
2021-08-02 18:11:04 +0800

    添加 excel 操作

---
+++ education/readExcel.py
@@ -0,0 +1,20 @@
+import openpyxl
+from bs4 import BeautifulSoup
+import urllib.request
+
+
+wb = openpyxl.Workbook()
+
+wb = openpyxl.load_workbook('C:\\Users\\codef\\Documents\\temp\\ceshi.xlsx')
+sh = wb['Sheet1']
+for i in range(1,3):
+    ce = sh.cell(row = i,column = 1)
+    url ="http://dict.youdao.com/search?q=" + ce.value
+    request = urllib.request.Request(url)
+    soup = BeautifulSoup(urllib.request.urlopen(url=request),features='html.parser')
+    yinbiao = soup.select(".phonetic")[0].text
+    sh.cell(row = i,column = 2).value = yinbiao
+    # print( soup.title)
+
+wb.save('C:\\Users\\codef\\Documents\\temp\\ceshi2.xlsx')
+wb.close()
