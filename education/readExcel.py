bda00ffc5e72d7d8a8479ec1a682e78884b90ffc
codefan <codefan@sina.com>
2021-08-04 15:55:15 +0800

    完善代码

--- education/readExcel.py
+++ education/readExcel.py
@@ -5,20 +5,21 @@ import urllib.request
 
 wb = openpyxl.Workbook()
 
-wb = openpyxl.load_workbook('C:\\Users\\codef\\Documents\\temp\\ceshi.xlsx')
+wb = openpyxl.load_workbook('/Users/codefan/Documents/temp/words_f_z.xlsx')
 sh = wb['Sheet1']
-for i in range(1,3):
-    ce = sh.cell(row = i,column = 1)
+for i in range(3,1594):
+    ce = sh.cell(row = i,column = 2)
     url ="http://dict.youdao.com/search?q=" + ce.value
     request = urllib.request.Request(url)
     soup = BeautifulSoup(urllib.request.urlopen(url=request),features='html.parser')
     yinbiao = soup.select(".phonetic")[0].text
-    sh.cell(row = i,column = 2).value = yinbiao
-    yinbiao = soup.select(".phonetic")[1].text
     sh.cell(row = i,column = 3).value = yinbiao
+    yinbiao = soup.select(".phonetic")[1].text
+    sh.cell(row = i,column = 4).value = yinbiao
     # print( soup.title)
     yinbiao = soup.select(".trans-container")[0].text
-    sh.cell(row = i,column = 4).value = yinbiao
+    if len(yinbiao) > 0:
+        sh.cell(row = i,column = 5).value = yinbiao
 
-wb.save('C:\\Users\\codef\\Documents\\temp\\ceshi2.xlsx')
+wb.save('/Users/codefan/Documents/temp/words_f_z2.xlsx')
 wb.close()
